8162b2e83c199396ce9a72c2441e4cf929cad04a
Unweldify <AILALAH@yandex.ru>
2024-10-16 18:43:45 +0400

    Added abstract class and type annotations

--- main.py
+++ main.py
@@ -1,87 +1,104 @@
-import abc, random
+import abc
+import random
 
-class Website:    
+
+class Website:
     def __init__(self, title: str, session_id: int):
         self._title = title
         self.session_id = session_id
-        self.__CSRF_token = self.__token_generate()
-
+        self.__csrf_token = self.__token_generate()
 
     @staticmethod
     def __token_generate() -> str:
         symbol_dictionary = "qwertyuiopasdfghjklzxcvbnm1234567890"
         symbol_dictionary += symbol_dictionary.upper()
 
-        CSRF_token = ""
+        csrf_token = ""
 
         for _ in range(32):
-            CSRF_token += random.choice(symbol_dictionary)
+            csrf_token += random.choice(symbol_dictionary)
 
-        return CSRF_token
-
+        return csrf_token
 
     @property
-    def CSRF_token(self) -> str:
-        return self.__CSRF_token
-
+    def csrf_token(self) -> str:
+        return self.__csrf_token
 
     @property
     def title(self) -> str:
         return self._title
-
 
     @title.setter
     def title(self, title: str) -> None:
         self._title = title
 
 
-class Login_page(Website):
+class LoginPage(Website):
     def __init__(self, title: str, session_id: int):
         super().__init__(title, session_id)
         self.title = self.title + " - Login"
 
     @property
     def title(self) -> str:
-        return self._title 
+        return self._title
 
     @title.setter
     def title(self, title: str) -> None:
         self._title = title + " - Login"
-        
-    
-    def log_in(self, username, password):
-        return f'validate:{username}:{password}:{self.CSRF_token}' # так как у меня нет целого рабочего сайта то будет просто возврат данных
 
-class Register_page(Website):
-    def __init__(self, title, session_id):
+    def log_in(self, username: str, password: str) -> str:
+        return f'validate:{username}:{password}:{self.csrf_token}'
+        # так как у меня нет целого рабочего сайта 
+        # то будет просто возврат данных
+
+
+class RegisterPage(Website):
+    def __init__(self, title: str, session_id: int):
         super().__init__(title, session_id)
         self.title = self.title + " - Register"
 
     @property
     def title(self) -> str:
-        return self._title 
+        return self._title
 
     @title.setter
     def title(self, title: str) -> None:
         self._title = title + " - Register"
 
-    def register(self, username, password, password_confirm):
-        if password == password_confirm:
-            return  f'reguser:{username}:{password}:{self.CSRF_token}' # аналогичная ситуация как и с логином
+    def register(self, username: str, password: str, confirm: str) -> str:
+        if password == confirm:
+            return f'reguser:{username}:{password}:{self.csrf_token}'
         return "Passwords don't match."
+        # аналогичная ситуация как и с логином
+
+
+class Redirector(abc.ABC, Website):
+    def __init__(self, session_id: str):
+        self.session_id = session_id
+        self.__csrf_token = self.__token_generate()
+
+    @abc.abstractmethod
+    def redirect(self, link: str) -> str:
+        pass
+
+
+class LoginRedirector(Redirector):
+    def __init__(self, session_id: str, token: str):
+        super().__init__(session_id, token)
+
+    def redirect(self, link: str) -> str:
+        return f'redirect:{link}'
 
 
 #
 
 website_name = "Webpage"
 website = Website(website_name, 1)
-log_page = Login_page(website_name, 1)
-reg_page = Register_page(website_name, 1)
+log_page = LoginPage(website_name, 1)
+reg_page = RegisterPage(website_name, 1)
 
 log_page.title = "Webbypage"
-print(log_page)
 print(log_page.log_in("User", "Pass"))
-
 
 reg_page.title = "Webpaaaage"
 print(reg_page.title)
